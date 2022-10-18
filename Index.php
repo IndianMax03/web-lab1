fdb6b2901f72933dc8dfa0305803f91b1ca12e06
IndianMax <58441613+IndianMax03@users.noreply.github.com>
2022-10-18 08:34:23 +0300

    added YoptaScript supporting

--- Index.php
+++ Index.php
@@ -1,1 +1,18 @@
-<?php
+<!DOCTYPE html>
+
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <title>Лабораторная №1</title>
+</head>
+
+<body>
+    <script language="YoptaScript">
+        шухер("Подключил какой-то скрипт") нах
+    </script>
+
+    <script src="yopta.js"></script>
+</body>
+
+</html>
